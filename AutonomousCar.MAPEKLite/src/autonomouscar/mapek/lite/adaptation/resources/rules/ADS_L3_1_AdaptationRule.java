f1b29bccae70ea7951ab0f3c22b55951443f9fcb
JosepDev23 <josepdev23@gmail.com>
2025-05-21 18:24:11 +0200

    idk

--- AutonomousCar.MAPEKLite/src/autonomouscar/mapek/lite/adaptation/resources/rules/ADS_L3_1_AdaptationRule.java
+++ AutonomousCar.MAPEKLite/src/autonomouscar/mapek/lite/adaptation/resources/rules/ADS_L3_1_AdaptationRule.java
@@ -3,14 +3,15 @@ package autonomouscar.mapek.lite.adaptation.resources.rules;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceReference;
 
+import es.upv.pros.tatami.adaptation.mapek.lite.ARC.structures.systemconfiguration.interfaces.IRuleComponentsSystemConfiguration;
 import es.upv.pros.tatami.adaptation.mapek.lite.artifacts.components.AdaptationRule;
 import es.upv.pros.tatami.adaptation.mapek.lite.artifacts.interfaces.IKnowledgeProperty;
 import es.upv.pros.tatami.adaptation.mapek.lite.helpers.BasicMAPEKLiteLoopHelper;
 import es.upv.pros.tatami.adaptation.mapek.lite.helpers.SystemConfigurationHelper;
 import es.upv.pros.tatami.adaptation.mapek.lite.structures.systemconfiguration.interfaces.IRuleSystemConfiguration;
-import es.upv.pros.tatami.osgi.utils.components.OSGiUtils;
 import es.upv.pros.tatami.osgi.utils.interfaces.ITimeStamped;
-import sua.autonomouscar.infraestructure.devices.ARC.RoadSensorARC;
+import sua.autonomouscar.infraestructure.devices.ARC.DistanceSensorARC;
+import sua.autonomouscar.infraestructure.devices.ARC.EngineARC;
 import sua.autonomouscar.interfaces.ERoadType;
 
 public class ADS_L3_1_AdaptationRule extends AdaptationRule {
@@ -39,9 +40,11 @@ public class ADS_L3_1_AdaptationRule extends AdaptationRule {
 	public IRuleSystemConfiguration onExecute(IKnowledgeProperty property) {
 		if (kp_RoadType.getValue().equals(ERoadType.STD_ROAD) 
 		|| kp_RoadType.getValue().equals(ERoadType.OFF_ROAD)) {
-			ServiceReference<?> ref_RoadSensorARC = context.getServiceReference(RoadSensorARC.class.getName());
-			if (ref_RoadSensorARC != null) {
-				
+			IRuleComponentsSystemConfiguration theNextSystemConfiguration = SystemConfigurationHelper
+					.createPartialSystemConfiguration(this.getId() + "_" + ITimeStamped.getCurrentTimeStamp());
+			
+			if (checkL2ACAvailability()) {
+				SystemConfigurationHelper.componentToAdd(theNextSystemConfiguration, "device.engine", EngineARC.PROVIDED_DEVICE);
 			}
 			
 		}
@@ -49,4 +52,12 @@ public class ADS_L3_1_AdaptationRule extends AdaptationRule {
 		return SystemConfigurationHelper
 				.createPartialSystemConfiguration(this.getId() + "_" + ITimeStamped.getCurrentTimeStamp());
 	}
+	
+	private boolean checkL2ACAvailability() {
+		ServiceReference<?> ref_EngineARC = context.getServiceReference(EngineARC.class.getName());
+		ServiceReference<?> ref_DistanceSensorARC = context.getServiceReference(DistanceSensorARC.class.getName() +".FrontDistanceSensor");
+		
+		return ref_EngineARC != null && ref_DistanceSensorARC != null;
+    }
+	
 }
